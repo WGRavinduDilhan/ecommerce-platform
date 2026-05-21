210987264831052d956db03fe16b517e5eac9220
Ravindu <dilhanw-ct21062@stu.kln.ac.lk>
2026-05-21 20:58:04 +0530

    Refactor indentation in Terraform configuration for consistency fixed indentatins

--- infra/main.tf
+++ infra/main.tf
@@ -1,22 +1,22 @@
 terraform {
-    required_version = ">= 1.7"
+  required_version = ">= 1.7"
 
-    required_providers {
-        google = {
-            source  = "hashicorp/google"
-            version = ">= 5.0"
-        }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
     }
+  }
 
-    backend "gcs" {
-        bucket = "<GCP_PROJECT_ID>-tfstate"
-        prefix = "terraform/state"
-    }
+  backend "gcs" {
+    bucket = "<GCP_PROJECT_ID>-tfstate"
+    prefix = "terraform/state"
+  }
 }
 
 # Configure the Google Provider
 
 provider "google" {
-    project = "gcp-project-id"
-    region  = "us-central1"
+  project = "gcp-project-id"
+  region  = "us-central1"
 }
